aeefe46512a3697a4a21321efd87b7ac3ce9c6b1
Picklegunner <rvs20hw@gmail.com>
2018-02-24 16:24:53 -0500

    Loads in JSON file and prints all user messages grouped by user.
    TODO set file on command line
    TODO add analysis

--- discordAnalysis.py
+++ discordAnalysis.py
@@ -1,8 +1,53 @@
 import json
 from pprint import pprint
 
-json_disc = json.load(open("dht.txt"))
+class User():
 
-pprint(json_disc)
+    id = 0
+    name = ''
+    messages = []
+
+    def __init__(self, name, id):
+        self.id = id
+        self.name = name
+
+    def add_message(self, message):
+        self.messages.append(message)
+
+    def print_all(self):
+        for message in self.messages:
+            print(message)
+
+class DiscordData():
+
+    users = []
+    channels = []
+
+    def __init__(self, file):
+
+        logs = json.load(open(file))
+
+        # populate user list
+        for index, id in enumerate(logs['meta']['userindex']): 
+            name = logs['meta']['users'][id]['name']
+            self.users.insert(index,User(name, id))
+
+        # add channel ids
+        self.channels = list(logs['meta']['channels'])
+
+        # add messages to users
+        for channel in self.channels:
+            for message_id in logs['data'][channel]:
+                user = logs['data'][channel][message_id]['u']
+                message = logs['data'][channel][message_id]['m']
+                self.users[user].add_message(message)
+
+    def print_all(self):
+        for user in self.users:
+            print(user.name)
+            user.print_all()
 
 
+
+data = DiscordData('rst.txt')
+data.print_all()
